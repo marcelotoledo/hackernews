d8eb52bf69b56f83abb9441f297c6ed05b433bc1
Marcelo Toledo <marcelo@marcelotoledo.com>
2019-03-05 21:17:13 -0300

    First import.

---
+++ lib/src/resources/news_api_provider.dart
@@ -0,0 +1,19 @@
+import 'package:http/http.dart' show Client;
+import 'dart:convert';
+
+final _root = 'https://hacker-news.firebaseio.com/v0';
+
+class NewsApiProvider {
+  Client client = Client();
+
+  fetchTopIds() async {
+    final response = await client.get('$_root/topstories.json');
+    final ids = json.decode(response.body);
+
+    return ids;
+  }
+
+  fetchItem(int id) async {
+    final response = await client.get('$_root/item/$id.json');
+  }
+}
