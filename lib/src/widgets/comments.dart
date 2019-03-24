8fe37e7571570e63e6979cef0d763eefefdf437c
Marcelo Toledo <marcelo@marcelotoledo.com>
2019-03-24 19:00:39 -0300

    Styling comment

--- lib/src/widgets/comments.dart
+++ lib/src/widgets/comments.dart
@@ -15,10 +15,15 @@ class Comment extends StatelessWidget {
           return Text('Still loading comment');
         }
 
+      final item = snapshot.data;
       final children = <Widget> [
-        Text(snapshot.data.text),
+        ListTile(
+          title: Text(item.text),
+          subtitle: Text(item.by),
+        ),
+        Divider(),
       ];
-      snapshot.data.kids.forEach((kidId) {
+      item.kids.forEach((kidId) {
         children.add(Comment(itemId: kidId, itemMap: itemMap));
       });
 
